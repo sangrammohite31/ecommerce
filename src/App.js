c427d6c3ff6b294a439fc1e85b327e181f326237
Sangram <sangrammohite280@gmail.com>
2024-05-18 12:30:52 +0530

    responsive menu

--- src/App.js
+++ src/App.js
@@ -6,15 +6,30 @@ let go = ()=>{
 }
 function App() {
 
-  let[status, setsatus ] = useState(false);
+ 
 
   let [menustatus , setmenustatus] = useState(false);
-
+  let [modalstatus,setmmodalstatus] =useState(false);
   return (
+    
     <div className="App">
-      <div className='menu'></div>
-   <input type={status?"name":"password"}></input><button className='bg-[red]' onClick={()=>setsatus(!status)}>{status?"hide":"Show"}</button>
-    </div>
+      <button className='enbtn' onClick={()=>setmmodalstatus(!modalstatus)}> Enquiry</button>
+      <div className={`overlay ${modalstatus?"showoverlay":""}`}></div>
+      <div className={`modaldiv  ${modalstatus?"showmodaldiv":""}`}>
+        <h1>Enquiry honw</h1>
+      </div>
+      <button className='micon' onClick={()=>setmenustatus(!menustatus)}>
+        {menustatus?<span className='times'>&times;</span>:<span>&#9776;</span>}</button>
+      <div className={`menu ${menustatus? "activemenu":""}`}>
+        <ul>
+          <li>home</li>
+          <li>Contact</li>
+          <li>Gallery</li>
+          <li>About</li>
+
+        </ul>
+      </div>
+     </div>
   );
 }
 

360c1fc559e22471d4f482963f050cb795d1e01c
Sangram <sangrammohite280@gmail.com>
2024-05-18 13:05:10 +0530

    header.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Menuu}from "./header.jsx";
 import { useState } from 'react';
 let go = ()=>{
   alert ("welocme to my first app")
@@ -8,27 +9,10 @@ function App() {
 
  
 
-  let [menustatus , setmenustatus] = useState(false);
-  let [modalstatus,setmmodalstatus] =useState(false);
-  return (
-    
+ return (
+   
     <div className="App">
-      <button className='enbtn' onClick={()=>setmmodalstatus(!modalstatus)}> Enquiry</button>
-      <div className={`overlay ${modalstatus?"showoverlay":""}`}></div>
-      <div className={`modaldiv  ${modalstatus?"showmodaldiv":""}`}>
-        <h1>Enquiry honw</h1>
-      </div>
-      <button className='micon' onClick={()=>setmenustatus(!menustatus)}>
-        {menustatus?<span className='times'>&times;</span>:<span>&#9776;</span>}</button>
-      <div className={`menu ${menustatus? "activemenu":""}`}>
-        <ul>
-          <li>home</li>
-          <li>Contact</li>
-          <li>Gallery</li>
-          <li>About</li>
-
-        </ul>
-      </div>
+    <Menuu/>
      </div>
   );
 }
--- src/header.jsx
+++ src/header.jsx
@@ -1,4 +1,27 @@
-export const cal =(v)=>{
-    return 12;
 
+import { useState } from 'react';
+export function Menuu(){
+    let [menustatus , setmenustatus] = useState(false);
+    let [modalstatus,setmmodalstatus] =useState(false);
+    
+    return (
+        <div className="menunquiry">
+              <button className='enbtn' onClick={()=>setmmodalstatus(!modalstatus)}> {modalstatus?"":"Enquiry"}</button>
+      <div onClick={()=>setmmodalstatus(!modalstatus)} className= {`overlay showoverlay}`}></div>
+      <div className={`modaldiv  ${modalstatus?"showmodaldiv":""}`}>
+        <h1>Enquiry honw</h1>
+      </div>
+      <button className='micon' onClick={()=>setmenustatus(!menustatus)}>
+        {menustatus?<span className='times'>&times;</span>:<span>&#9776;</span>}</button>
+      <div className={`menu ${menustatus? "activemenu":""}`}>
+        <ul>
+          <li>home</li>
+          <li>Contact</li>
+          <li>Gallery</li>
+          <li>About</li>
+
+        </ul>
+      </div>
+        </div>
+    );
 }
